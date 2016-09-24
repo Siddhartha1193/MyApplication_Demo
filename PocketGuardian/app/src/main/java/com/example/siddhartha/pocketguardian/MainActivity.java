24b3e016765d9d1383ad025336d8dd22d13456fe
SIDDHARTHA <ssiddhartha.1193@gmail.com>
2016-09-24 00:24:13 -0700

    added new files

---
+++ PocketGuardian/app/src/main/java/com/example/siddhartha/pocketguardian/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.siddhartha.pocketguardian;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
